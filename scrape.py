0e1ab9b45e66f19ef6df3c54654fb3a5605ae4f6
aacb88 <arthur.cook@gmail.com>
2015-10-18 18:56:15 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -5,15 +5,6 @@ from bs4 import BeautifulSoup
 br = mechanize.Browser()
 br.open('http://enrarchives.sos.mo.gov/enrnet/')
 
-#Fill out the form
-br.select_form('Form1')
-print form
+br.select_form(id="Form1")
 
-
-  
-#Submit the form
-#br.submit()
-
-#html = br.response().read()
-
-#print html
+print br.form
