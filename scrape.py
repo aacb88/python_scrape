a6856a7159ef369945336af6590a37f8c40a4772
aacb88 <arthur.cook@gmail.com>
2015-10-18 17:20:37 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,6 +6,12 @@ br = mechanize.Browser()
 br.open('http://enrarchives.sos.mo.gov/enrnet/')
 
 #Fill out the form
-
+if br.form["MainContent_cboRaces"]== ["------Missouri Statewide Offices-----"]:
+  br.form["MainContent_cboRaces"].value = ["460006719"]
+  
 #Submit the form
 br.submit()
+
+html = br.response().read()
+
+print html
