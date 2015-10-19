2f07d2d1ce85a51ec1c779aaabcde91ea9642041
aacb88 <arthur.cook@gmail.com>
2015-10-18 19:15:15 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,4 +7,10 @@ br.open('http://enrarchives.sos.mo.gov/enrnet/')
 
 br.select_form(nr=0)
 
-print br.form
+control = br.form.find_control("MainContent_cboElectionNames")
+
+for item in control.items:
+  if item.name == "750003143":
+    item.selected = True
+    print item
+  
