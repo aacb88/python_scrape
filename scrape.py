d7eb98c28d2a001043d7464a45c6e50d2e4a3cf9
aacb88 <arthur.cook@gmail.com>
2015-10-18 19:21:23 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -12,5 +12,10 @@ control = br.form.find_control(id = "MainContent_cboRaces")
 for item in control.items:
   if item.name == "750003143":
     item.selected = True
-    print item
+  
+  response = br.submit(nr=1)
+  
+  html = br.response().read()
+  
+  print html
   
