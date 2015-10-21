2c147c9e7e93634c1f01308ae328bbee8876fe68
aacb88 <arthur.cook@gmail.com>
2015-10-20 19:33:07 -0500

    testing

--- scrape2.py
+++ scrape2.py
@@ -1,6 +1,7 @@
 import urllib2, csv
 import mechanize
 from bs4 import BeautifulSoup
+#reading unicode
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -8,15 +9,18 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.open('http://enrarchives.sos.mo.gov/enrnet/')
 
+#selecting the first form 
 br.select_form(nr=0)
 
+#Selecting Auditor Race for Main Page
 control = br.form.find_control(id = "MainContent_cboRaces")
 
 for item in control.items:
-  if item.name == "460006719":
-    item.selected = True
-  
-    response = br.submit(nr=1)
+    if item.name == "460006719":
+        item.selected = True
+
+#Hit second submit button  
+response = br.submit(nr=1)
 
 html = br.response().read()
 
@@ -24,32 +28,19 @@ soup = BeautifulSoup(html, "html.parser")
 
 main_table = soup.find('table', {'id': 'MainContent_dgrdRaceResults'})
 
-
-candidate_data = []
-#for row in main_table.find_all('tr'):
-  #data = [cell.text for cell in row.find_all('td')]
-  #candidate_data.append(data)
+#Creating Output
+county_data = []
 
 county_table =soup.find('table', {'id': 'MainContent_dgrdCountyRaceResults'})
 
-headers = county_table.find('tr', [0])
-
-for row in headers:
-  fields = [cell.text for cell in row.find_all('th')]
-  candidate_data.append(fields)
-
-#for tr[0] in county_table.find_all('th'):
-    #field_name = [cell.text for cell in header.find_all('th')]
-    #candidate_data.append(field_name)
-
-#for header in county_table.find_all('tr'):
-  #field_name = [cell.text for cell in header.find_all('th')]
-  #candidate_data.append(field_name)
-
 for row in county_table.find_all('tr'):
-  data = [cell.text for cell in row.find_all('td')]
-  candidate_data.append(data)
+    loop_box = []
+    headers = [cell.text for cell in row.find_all('th')]
+    loop_box.append(headers)
+    data = [cell.text for cell in row.find_all('td')]
+    loop_box.append(data)
+    county_data.append(loop_box)
 
-outfile = open("./candidate_table.csv", "w")
+outfile = open("./county_table.csv", "w")
 writer = csv.writer(outfile)
-writer.writerows(candidate_data)
+writer.writerows(county_data)
